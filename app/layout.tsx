ef6a1f3c346ba9e53c1e2fff1b7b597519c3df9e
ichiratin123 <phonghuynh1501@gmail.com>
2026-01-04 11:19:24 +0700

    add store/auth_bug/loader

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,23 +4,35 @@ import "./globals.css";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import BackToTopButton from "./components/BackToTop";
+import { Provider } from "react-redux";
+import { store } from "@/app/store/store";
 
 config.autoAddCss = false;
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
-const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "LockWind | Accommodation Booking Platform",
-  description: "LockWind is a short-term accommodation booking platform developed by Loc & Phong.",
+  description:
+    "LockWind is a short-term accommodation booking platform developed by Loc & Phong.",
   icons: { icon: "/icon.png" },
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        {children}
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <Provider store={store}>{children}</Provider>
         <BackToTopButton />
       </body>
     </html>
